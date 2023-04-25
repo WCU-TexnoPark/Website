f267a9019f48702149122fd27e08f16a09ba33e1
Qiyas <qiyasturk@gmail.com>
2023-04-25 20:22:58 +0400

    2

--- TexnoPark/Pages/Main/js/main.js
+++ TexnoPark/Pages/Main/js/main.js
@@ -1,1 +1,8 @@
-
+// For Fun
+let titlee = document.title;
+window.addEventListener("blur", () => {
+    document.title = "TexnoParka geri dön :(";
+});
+window.addEventListener("focus", () => {
+    document.title = titlee;
+});
