05d889ac9ce794f7d00cc97881becbb5518d5c91
Qiyas <qiyasturk@gmail.com>
2023-04-26 22:01:59 +0400

    a

--- TexnoPark/Pages/Main/js/main.js
+++ TexnoPark/Pages/Main/js/main.js
@@ -6,3 +6,22 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
     document.title = titlee;
 });
+
+// Navbar Logo Width for dordbucaq
+let navbarLogo = document.querySelector('.navbar-texno-logo');
+let navbarLogoHeight = navbarLogo.clientHeight;
+navbarLogo.style.width = navbarLogoHeight + 'px';
+
+
+// TexnoPark text font size
+window.addEventListener('resize', function() {
+    let texnoParkTextFirstPage = document.getElementsByClassName('first-screen-header');
+    let pageMaxWidth = 600;
+    let width = window.innerWidth;
+    for (let i = 0; i < texnoParkTextFirstPage.length; i++) {
+      let currentFontSize = parseInt(window.getComputedStyle(texnoParkTextFirstPage[i]).getPropertyValue('font-size'));
+      let newFontSize = (width <= pageMaxWidth) ? currentFontSize * (width / pageMaxWidth) : currentFontSize;
+      texnoParkTextFirstPage[i].style.fontSize = newFontSize + 'px';
+    }
+  });
+  
